ce2286cb1d020963322294fc7f94af1c999c6f2a
Guigen.Wang <guigen.wang.cn@gmail.com>
2016-08-15 17:30:22 +0800

    创建单例的sessionFactory

---
+++ src/com/aaa/db/MyHibernateSessionFactory.java
@@ -0,0 +1,24 @@
+package com.aaa.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class MyHibernateSessionFactory {
+	private static SessionFactory sessionFactory;//会话工厂属性
+
+	//单例模式下，构造器为private
+	private MyHibernateSessionFactory() {
+	}
+	//静态方法创建单例的sessionFactory
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null) {
+			Configuration configuration = new Configuration().configure();
+			sessionFactory = configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build());
+			return sessionFactory;
+		} else {
+			return sessionFactory;
+		}
+	}
+}
